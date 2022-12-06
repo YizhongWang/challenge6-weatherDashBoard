ad365c8b206752c4d127e96b1618e7ae488d3bad
Yizhong Wang <wang.6689@osu.edu>
2022-12-05 22:37:45 -0500

    city

--- script.js
+++ script.js
@@ -1,11 +1,40 @@
 var apiKey = "af44960abbcc41f5ff680fae977d1d71";
 var button = $("#search-button");
-var searchInput = "";
+var forecastApi = "https://api.openweathermap.org/data/2.5/forecast";
+var weatherApi="https://api.openweathermap.org/data/2.5/weather";
 
 
 button.click(function (){
-console.log("button was clicked");
-searchInput = $("#search-city").val().trim();
-console.log(searchInput);
-
+    console.log("button was clicked");
+    var searchInput = $("#search-city").val().trim();
+    console.log(searchInput);
+    getCity(searchInput)
+        .then(getWeather);
 })
+
+function getCity(searchInput){
+    var apiUrl = weatherApi + "?q=" + searchInput + "&appid=" + apiKey +"&units=imperial";
+    return fetch(apiUrl)
+        .then(function(response){
+           if(response.ok){
+                return response.json();
+           }
+        })
+}  
+
+function getWeather(cityData){
+    console.log(cityData);
+    var lat = cityData.coord.lat;
+    var lon = cityData.coord.lon;
+    var apiUrl = forecastApi + "?lat=" + lat + "&lon=" + lon + "&appid=" + apiKey + "&units=imperial";
+    console.log(apiUrl);
+    fetch(apiUrl)
+        .then(function(response){
+            if(response.ok){
+                return response.json()
+            }
+        })
+        .then(function(data){
+            console.log(data);
+        });
+}
